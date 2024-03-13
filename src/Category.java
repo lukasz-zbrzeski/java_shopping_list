20578204bb5f35518a5c48c12cbbbad454cdda35
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2024-03-13 17:50:52 +0100

    Add categoryName and getter, setter

--- src/Category.java
+++ src/Category.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 
 public class Category {
+    private String categoryName;
     private ArrayList<Product> products;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 
     public ArrayList<Product> getProducts() {
         return products;
